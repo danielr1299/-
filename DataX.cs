603105ee87a8d958757c9267edfbcb8b662ad739
danielr1299 <90899547+danielr1299@users.noreply.github.com>
2022-02-17 16:47:16 +0200

    Add files via upload

---
+++ DataX.cs
@@ -0,0 +1,26 @@
+﻿using DataStuctures;
+using System;
+
+namespace Logic.InnerDataTypes
+{
+    public class DataX : IComparable<DataX>
+    {   
+        public double X { get; set; }
+
+        public BST<DataY> DataY {get; set; }
+
+        public DataX(double x,double y)
+        {
+            BST<DataY> data = new BST<DataY>();
+            data.AddY(new DataY(y) { Y = y });
+            X = x;
+        }
+
+        public int CompareTo(DataX other)
+        {
+            return X.CompareTo(other.X);
+        }       
+    }
+
+    
+}
---
+++ DataY.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Logic.InnerDataTypes
+{
+    public class DataY : IComparable<DataY>
+    {
+        public double Y {get; set; }
+        public int Count { get; set; }
+
+        public DataY(double y)
+        {
+            Y = y;
+        }
+        
+
+        public int CompareTo(DataY other)
+        {
+            return Y.CompareTo(other.Y);
+        }
+    }
+}
